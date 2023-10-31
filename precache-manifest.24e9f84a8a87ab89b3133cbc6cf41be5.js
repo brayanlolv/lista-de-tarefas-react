4c45c06c805f1af7ed359188ef33589b3b577142
brayan <brayanlucasop@gmail.com>
2023-10-31 14:34:39 -0300

    Updates

--- precache-manifest.24e9f84a8a87ab89b3133cbc6cf41be5.js
+++ precache-manifest.24e9f84a8a87ab89b3133cbc6cf41be5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8531437811ef3c85d93af9a7d722653d",
+    "revision": "23905fc41eaa5737583ae1ddba10c4b2",
     "url": "/lista-de-tarefas/index.html"
   },
   {
-    "revision": "5310aa0a5c1b8443e676",
+    "revision": "4a9a3b6ee419a298f412",
     "url": "/lista-de-tarefas/static/css/main.2624d9f6.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/lista-de-tarefas/static/js/2.a4869783.chunk.js"
   },
   {
-    "revision": "5310aa0a5c1b8443e676",
-    "url": "/lista-de-tarefas/static/js/main.7c75a73b.chunk.js"
+    "revision": "4a9a3b6ee419a298f412",
+    "url": "/lista-de-tarefas/static/js/main.dd14046c.chunk.js"
   },
   {
     "revision": "fbb1b0fe63695093254a",
